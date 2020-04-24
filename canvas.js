cdaa7e5649e2e908cbb93432db2cd3aa841c0012
Nick Minor <nickminor1991@gmail.com>
2020-04-24 14:28:20 -0600

    complete

--- canvas.js
+++ canvas.js
@@ -5,18 +5,45 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+var mouse = {
+  x: undefined,
+  y: undefined
+}
+var maxRadius = 30;
+
+var colorArray = [
+  '#FCA27C',
+  '#FFCF96',
+  '#FCF79F',
+  '#B4D47B',
+  '#82BF65',
+];
+
+window.addEventListener('mousemove', function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  console.log(mouse);
+});
+
+window.addEventListener('resize', function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
 function Circle(x, y, dx, dy, radius) {
   this.x = x;
   this.y = y;
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)]
+  this.minRadius = radius;
 
   this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.strokeStyle = 'black';
-    c.stroke();
+    c.fillStyle = this.color;
+    c.fill();
   }
   this.update = function () {
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
@@ -27,6 +54,15 @@ function Circle(x, y, dx, dy, radius) {
     }
     this.x += this.dx;
     this.y += this.dy;
+
+    //interactivity
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50
+      && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+      if (this.radius < maxRadius) { this.radius += 1 }
+    }
+    else if (this.radius > this.minRadius) {
+      this.radius -= 1;
+    }
     this.draw();
 
   }
@@ -38,12 +74,12 @@ function Circle(x, y, dx, dy, radius) {
 // var radius = 30;
 var circleArray = [];
 
-for (var i = 0; i < 50; i++) {
-  var radius = 30;
+for (var i = 0; i < 800; i++) {
+  var radius = Math.random() * 3 + 1;
   var x = Math.random() * (innerWidth - radius * 2) + radius;
   var y = Math.random() * (innerHeight - radius * 2) + radius;
-  var dx = (Math.random() - 0.5);
-  var dy = (Math.random() - 0.5);
+  var dx = (Math.random() - .5);
+  var dy = (Math.random() - .5);
   circleArray.push(new Circle(x, y, dx, dy, radius));
 }
 console.log(circleArray);
